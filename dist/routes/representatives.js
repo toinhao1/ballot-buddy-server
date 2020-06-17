b9babbd8f5aeeffc3e3afa57a2881d157f14a53a
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2020-06-17 21:08:16 +0200

    Able to get list of current reps

--- dist/routes/representatives.js
+++ dist/routes/representatives.js
@@ -8,13 +8,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const passport_1 = require("passport");
 const vote_smart_1 = require("../controllers/vote-smart");
+const User_1 = __importDefault(require("../models/User"));
 const representativeRouter = express_1.Router();
-representativeRouter.get('/current-representatives', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { zipcode, zipcode4 } = req.body;
-    const data = yield vote_smart_1.getCurrentRepresentatives(zipcode, zipcode4);
-    res.send({ message: "Here are your reps!", data });
+representativeRouter.get('/current-representatives', passport_1.authenticate('jwt', { session: false }), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    if (req.user) {
+        // get the user
+        const user = yield User_1.default.findById(req.user.id);
+        // extract zipcode
+        const { zipcode, plusFourZip } = (_a = user) === null || _a === void 0 ? void 0 : _a.address;
+        // get the current reps from votesmart
+        const data = yield vote_smart_1.getCurrentRepresentatives(zipcode, plusFourZip);
+        res.status(200).send({ message: "Here are your reps!", data });
+    }
+    else {
+        res.send("Fuck off");
+    }
 }));
 exports.default = representativeRouter;
--- src/routes/representatives.ts
+++ src/routes/representatives.ts
@@ -2,16 +2,25 @@ import { Router, Request, Response } from 'express'
 import { authenticate } from 'passport'
 
 import { getCurrentRepresentatives } from '../controllers/vote-smart'
+import User from '../models/User'
 
 
 const representativeRouter = Router()
 
-representativeRouter.get('/current-representatives', async (req: Request, res: Response) => {
-  const { zipcode, zipcode4 } = req.body
+representativeRouter.get('/current-representatives', authenticate('jwt', { session: false }), async (req: Request, res: Response) => {
+  if (req.user) {
+    // get the user
+    const user = await User.findById(req.user.id)
+    // extract zipcode
+    const { zipcode, plusFourZip } = user?.address
+    // get the current reps from votesmart
+    const data = await getCurrentRepresentatives(zipcode, plusFourZip)
 
-  const data = await getCurrentRepresentatives(zipcode, zipcode4)
+    res.status(200).send({ message: "Here are your reps!", data })
 
-  res.send({ message: "Here are your reps!", data })
+  } else {
+    res.send("Fuck off")
+  }
 })
 
 

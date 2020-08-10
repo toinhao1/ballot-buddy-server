1c0b547bd0138da3f0ba74221912c49218fe08ec
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2020-08-10 14:48:09 +0200

    Clear data on an address changed implemented

--- dist/routes/address.js
+++ dist/routes/address.js
@@ -12,6 +12,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const passport_1 = require("passport");
 const smarty_streets_1 = require("../controllers/smarty-streets");
+const CurrentReps_1 = require("../models/CurrentReps");
+const Ballot_1 = require("../models/Ballot");
 const Address_1 = require("../models/Address");
 const User_1 = require("../models/User");
 const addressRouter = express_1.Router();
@@ -32,6 +34,9 @@ addressRouter.post('/set-address', passport_1.authenticate('jwt', { session: fal
         const combinedAddress = Object.assign(Object.assign({}, address), smartyStreetsData);
         // create the new address
         const currentAddress = new Address_1.Address(combinedAddress);
+        // when changing address remove current list of representatives
+        yield CurrentReps_1.CurrentReps.findOneAndDelete({ user: _id });
+        yield Ballot_1.Ballot.findOneAndDelete({ user: _id });
         const userToSave = yield User_1.User.findOne({ _id });
         if (!userToSave) {
             throw new Error('User not found');
